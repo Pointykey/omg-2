50d0fa9cc1ded991ad2f36f6f08c85786fb88136
Abu Bákr Siddique <siddiqiabubaker079@gmail.com>
2023-12-06 04:21:11 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,7 +2,8 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: 'omg-2.onrender.com',
-  port: '3001'
+  port: '3001',
+  secure: 'true'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
